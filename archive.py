262fb0c9ac9a47fb77f037bef21e43c216103012
Olexandr Savchuk <mail@olex.biz>
2011-11-25 20:28:18 +0100

    Added multiple date formats support

--- archive.py
+++ archive.py
@@ -6,20 +6,21 @@ from dateutil.relativedelta import relativedelta
 archive_from = "/home/vima/Picasa/VIMA"
 archive_to = "/home/vima/Picasa/VIMA/VIMA LAGER"
 
-d_format = "%d.%m.%y"
-
+d_formats = ["%d.%m.%y", "%d.%m.%Y", "%d-%m.%y", "%d-%m.%Y", "%d-%m-%y", "%d-%m-%Y"]
+	
 end = datetime.now() + relativedelta( days = -7 )
 
 log = open('archive-log.txt', 'w')
 print >>log, datetime.now(), 'Starting archiving...'
 
-for line in os.listdir(archive_from):
-    try:
-        date = datetime.strptime(line.split()[0], d_format)
-        if date < end:
-            shutil.move(archive_from+'/'+line, archive_to)
-            print >>log, 'Moving:', line
-    except (ValueError, IndexError):
-        pass 
+for d_format in d_formats:
+	for line in os.listdir(archive_from):
+		try:
+			date = datetime.strptime(line.split()[0], d_format)
+			if date < end:
+				shutil.move(archive_from+'/'+line, archive_to)
+				print >>log, 'Moving:', line
+		except (ValueError, IndexError):
+			pass 
 
 print >>log, datetime.now(), 'Archiving finished.'
