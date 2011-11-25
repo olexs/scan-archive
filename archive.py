8475e2596cb0d0efc95b3d666a7077488fb3bcff
Olexandr Savchuk <mail@olex.biz>
2011-11-25 19:20:39 +0100

    Added path variables

--- archive.py
+++ archive.py
@@ -2,12 +2,16 @@
 import sys, os
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+
+archive_from = "/home/vima/Picasa/VIMA"
+archive_to = "/home/vima/Picasa/VIMA/VIMA_LAGER"
+
 d_format = "%d.%m.%y"
 
 start = datetime.strptime('01.01.11', d_format)
 end = datetime.now() + relativedelta( days = -7 )
 
-for line in sys.stdin:
+for line in os.listdir(archive_from):
     try:
         date = datetime.strptime(line.split()[0], d_format)
         if start <= date < end:
